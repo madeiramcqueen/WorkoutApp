32769cd422ebc573b35d17c18d5fd0134c3a18ed
madeiramcqueen <madeira@mcqueen.net>
2022-12-02 11:37:12 -0800

    Add weight level and reps options to exercise page

--- src/pages/ExercisePage/ExercisePage.jsx
+++ src/pages/ExercisePage/ExercisePage.jsx
@@ -2,6 +2,37 @@ export default function ExercisePage() {
     return (
         <div className="ExercisePage">
             <h1>Select Your Exercise</h1>
+            <form>
+                <label>Weight Level</label>
+                <select>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="5">5</option>
+                    <option value="8">8</option>
+                    <option value="10">10</option>
+                    <option value="5">15</option>
+                    <option value="20">20</option>
+                    <option value="25">25</option>
+                    <option value="30">30</option>
+                    <option value="35">35</option>
+                    <option value="40">40</option>
+                    <option value="45">45</option>
+                    <option value="50">50</option>
+                </select>
+                <label>Number of Reps</label>
+                <select>
+                    <option value="5">5</option>
+                    <option value="10">10</option>
+                    <option value="15">15</option>
+                    <option value="20">20</option>
+                    <option value="25">25</option>
+                    <option value="30">30</option>
+                    <option value="35">35</option>
+                    <option value="40">40</option>
+                    <option value="45">45</option>
+                    <option value="50">50</option>
+                </select>
+            </form>
         </div>
 
     )
