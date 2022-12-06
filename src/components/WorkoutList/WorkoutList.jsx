9d1d9f09a407852a19254cdfce5a84d0cedfe54c
madeiramcqueen <madeira@mcqueen.net>
2022-12-06 11:34:40 -0800

    Add async function and useEffect to update workouts list on homepage

--- src/components/WorkoutList/WorkoutList.jsx
+++ src/components/WorkoutList/WorkoutList.jsx
@@ -1,10 +1,36 @@
-import App from "../../pages/App/App";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useInsertionEffect } from "react";
 
-export default function WorkoutList({ user, setUser }) {
+async function index() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve([{ key: 'Workout 1: ', exercises: [] }])
+        }, 1000)
+    })
+}
+
+export default function WorkoutList() {
+    const [workouts, setWorkouts] = useState([])
+    console.log('Workouts list/home page is rendering!')
+
+    useEffect(() => {
+        async function fetchWorkoutsAndUpdateState() {
+            const response = await index()
+            setWorkouts(response)
+        }
+        fetchWorkoutsAndUpdateState()
+    }, [])
+
+
     return (
         <div class="workout-list">
             <h2>Your current workouts:</h2>
+            {
+                workouts
+                    ? <p>{workouts.map(workout => <span key={workout.key}> {workout.key} </span>)}</p>
+                    : <p>Waiting for server...</p>
+            }
             <Link to={`/workouts`}>Create Your Workout!</Link>
         </div>
     )
