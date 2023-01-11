180ab9e6d9d0d5de5dcce7ff1dd7950dff9684ef
madeiramcqueen <madeira@mcqueen.net>
2023-01-11 09:00:57 -0800

    Add psdeudocode

--- routes/api/workouts.js
+++ routes/api/workouts.js
@@ -2,10 +2,19 @@ const express = require('express');
 const router = express.Router();
 const workoutCtrl = require('../../controllers/api/workouts');
 
+//Create a workout
 router.post('/', workoutCtrl.create);
+
+//Update a specific workout
 router.put('/:id', workoutCtrl.update);
+
+//Show workouts
 router.get('/', workoutCtrl.index);
+
+// Show a specific workout
 router.get('/:id', workoutCtrl.show);
+
+//Delete a specific workout
 router.delete('/:id', workoutCtrl.delete);
 
 module.exports = router;
